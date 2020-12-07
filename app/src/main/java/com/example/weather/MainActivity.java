2d565bb421fbb540d354ddae5ed5a54ab16f960e
murmusandeep <aaryapooh@gmail.com>
2020-12-07 20:36:03 +0530

    add current location function

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,9 +1,23 @@
 package com.example.weather;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -15,11 +29,15 @@ import com.example.weather.Retrofit.ApiInterface;
 import com.example.weather.Retrofit.WeatherData;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     EditText mCityName;
     ImageView mSearch;
@@ -29,10 +47,18 @@ public class MainActivity extends AppCompatActivity {
     TextView mText;
     ImageView mIcon;
 
+    TextView mCity;
+
+    LocationManager locationManager;
+
+    String city = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        grantPermission();
 
         mSearch = findViewById(R.id.search);
         mCityName = findViewById(R.id.cityName);
@@ -47,15 +73,78 @@ public class MainActivity extends AppCompatActivity {
         mText = findViewById(R.id.text);
         mIcon = findViewById(R.id.icon);
 
+        mCity = findViewById(R.id.city);
+
+        checkLocationIsEnableOrNot();
+        getLocation();
+
         mSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getWeatherData(mCityName.getText().toString().trim());
             }
         });
+    }
 
-        //  startActivity(new Intent(MainActivity.this, SecondActivity.class));
+    private void getLocation() {
 
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 500, 5, this);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void checkLocationIsEnableOrNot() {
+
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        boolean gpsEnable = false;
+        boolean networkEnable = false;
+
+        try {
+            gpsEnable = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            networkEnable = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (!gpsEnable && !networkEnable) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Enable GPS Service")
+                    .setCancelable(false)
+                    .setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    }).setNegativeButton("Cancel", null)
+                    .show();
+        }
+    }
+
+    private void grantPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+        }
     }
 
     private void getWeatherData(String name) {
@@ -70,17 +159,11 @@ public class MainActivity extends AppCompatActivity {
 
                 if (response.code() == 400) {
                     Toast.makeText(MainActivity.this, "You have not entered city name or Location not found.", Toast.LENGTH_SHORT).show();
-                }
-
-                else if(response.code() == 401) {
+                } else if (response.code() == 401) {
                     Toast.makeText(MainActivity.this, "API key provided is invalid or API key not provided.", Toast.LENGTH_SHORT).show();
-                }
-
-                else if (response.code() == 403) {
+                } else if (response.code() == 403) {
                     Toast.makeText(MainActivity.this, "API key has exceeded calls per month quota.", Toast.LENGTH_SHORT).show();
-                }
-
-                else {
+                } else {
 
                     mTemperature.setText("Temperature: " + response.body().getCurrent().getTemp() + "°C");
                     mWindSpeed.setText("Wind Speed: " + response.body().getCurrent().getWindSpeed() + " Km");
@@ -101,7 +184,7 @@ public class MainActivity extends AppCompatActivity {
 
                     //mDate.setText(response.body().getForecast().getForecastDayList().get(0).getDate());
 
-                // int size = response.body().getForecast().getForecastDayList().size();
+                    // int size = response.body().getForecast().getForecastDayList().size();
 
                 }
 
@@ -114,4 +197,34 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+
+        try {
+            Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+
+            mCity.setText(addresses.get(0).getLocality());
+
+            getWeatherData(addresses.get(0).getLocality());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
