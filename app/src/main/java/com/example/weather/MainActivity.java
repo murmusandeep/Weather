ad675a672ea63a508a0502d7c16a18b349e7cfdc
murmusandeep <aaryapooh@gmail.com>
2020-12-09 14:36:03 +0530

    Search not working

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.weather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -13,6 +16,7 @@ import android.widget.Toast;
 import com.example.weather.Retrofit.ApiClient;
 import com.example.weather.Retrofit.ApiInterface;
 import com.example.weather.Retrofit.WeatherData;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.squareup.picasso.Picasso;
 
 import retrofit2.Call;
@@ -30,9 +34,6 @@ public class MainActivity extends AppCompatActivity {
 
     TextView mText;
     ImageView mIcon;
-
-    ImageView mImage1;
-    TextView mMaxTemperature, mMinTemperature, mDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,19 +57,18 @@ public class MainActivity extends AppCompatActivity {
         mText = findViewById(R.id.text);
         mIcon = findViewById(R.id.icon);
 
-        mImage1 = findViewById(R.id.image1);
-        mMaxTemperature = findViewById(R.id.maxTemperature);
-        mMinTemperature = findViewById(R.id.minTemperature);
-        mDate = findViewById(R.id.date);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
-        mSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getWeatherData(mCityName.getText().toString().trim());
-            }
-        });
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
 
-        //  startActivity(new Intent(MainActivity.this, SecondActivity.class));
+//        mSearch.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                getWeatherData(mCityName.getText().toString().trim());
+//            }
+//        });
 
     }
 
@@ -130,5 +130,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+
+
+                            break;
+                        case R.id.search_city:
+                            selectedFragment = new SearchFragment();
+                            break;
+                        case R.id.setting:
+                            selectedFragment = new SettingFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 
 }
