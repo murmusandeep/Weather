0481fb7afa4ac0b159c05d537fac2e6b12f93058
murmusandeep <aaryapooh@gmail.com>
2020-12-09 17:13:20 +0530

    Search not working

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -141,8 +142,6 @@ public class MainActivity extends AppCompatActivity {
                     switch (item.getItemId()) {
                         case R.id.nav_home:
                             selectedFragment = new HomeFragment();
-
-
                             break;
                         case R.id.search_city:
                             selectedFragment = new SearchFragment();
